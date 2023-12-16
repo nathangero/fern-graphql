9cdd382d4259d2b95081401ea32398f2b11bd9b5
Nathan Geronimo <nathangero@yahoo.com>
2023-12-16 03:20:22 -0800

    fix using prod cloud func; add new scripts

--- src/App.jsx
+++ src/App.jsx
@@ -9,8 +9,8 @@ import './App.css'
 
 
 const client = new ApolloClient({
-  // uri: 'https://us-central1-fern-graphql-tester.cloudfunctions.net/graphql',
-  uri: 'http://localhost:5001/fern-graphql-tester/us-central1/graphql',
+  uri: 'https://us-central1-fern-graphql-tester.cloudfunctions.net/graphql',
+  // uri: 'http://localhost:5001/fern-graphql-tester/us-central1/graphql',
   cache: new InMemoryCache(),
 });
 
